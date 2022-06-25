cc7aead8f62c3b791e7d973f59ae8e7d360fba27
Yasmin Rodrigues <yasminrodsil@gmail.com>
2022-06-25 20:56:37 -0300

    first commit

---
+++ ex07.1.py
@@ -0,0 +1,5 @@
+
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+n =int(input('Digite um número:'))
+print('O antecessor desse número é', n-1)
+print('O sucessor desse número é', n+1)
