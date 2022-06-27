2f1c221438c21b25e12ff4a309183c46aa95fdf4
Yasmin Rodrigues <yasminrodsil@gmail.com>
2022-06-27 14:42:07 -0300

    first commit

--- ex07.1.py
+++ ex07.1.py
@@ -1,5 +1,4 @@
-
-#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
-n =int(input('Digite um número:'))
-print('O antecessor desse número é', n-1)
-print('O sucessor desse número é', n+1)
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e o seu antecessor.
+n =int(input('Digite um número inteiro:'))
+print('Seu antecessor é:{}'.format(n-1))
+print('Seu sucessor é:{}' .format(n+1))
